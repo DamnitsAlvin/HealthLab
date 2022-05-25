a200bc011991a1bc8cc41477e7c2c87678b931af
DamnitsAlvin <alvinlim794@gmail.com>
2022-05-26 00:37:16 +0800

    test

--- backend/run.py
+++ backend/run.py
@@ -3,12 +3,14 @@ from flask_cors import CORS, cross_origin
 from app import create_app
 from routes.user import api
 from routes.doctor import doc_api
+from routes.test import test_api
 from flask_jwt_extended import JWTManager
 
 app,mysql = create_app()
 jwt = JWTManager(app)
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(doc_api, url_prefix="/api")
+app.register_blueprint(test_api, url_prefix="/api")
 
 CORS(app, resources={r"/*": {"origins": "*"}}, headers='Content-Type')
 
--- front/src/screens/Testing.js
+++ front/src/screens/Testing.js
@@ -1,8 +1,11 @@
+import  Axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Testing(){
     const [position, setPosition] = useState({})
+    const [selectedFile, setSelectedFile] = useState()
+    const [isFileSelected, isSelectedFile] = useState(false)
     function getLocation(){
         if ("geolocation" in navigator) {
            navigator.geolocation.getCurrentPosition(function(position){
@@ -24,8 +27,31 @@ export default function Testing(){
         }
         getLocation()
     }, [id])
+
+
+    const changeHandler = (event) =>{
+        setSelectedFile(event.target.files[0])
+        isSelectedFile(true)
+    }
+
+    const submitHandler = async(event) =>{
+        event.preventDefault()
+        const formData = new FormData();
+		formData.append('file', selectedFile);
+        const {data} = await Axios.post("http://localhost:5000/api/test", formData)
+        console.log(data.message)
+
+    }
     return(
-        <div className='centerContainer'>{position.error ? position.error : position.latitude + " " + position.longitude}</div>
-        
+        <>
+            <div className='centerContainer'>{position.error ? position.error : position.latitude + " " + position.longitude}
+            <form encType="multipart/form-data" onSubmit={submitHandler}>
+                <input type="file" name="file" onChange={changeHandler}/>
+                <input type="submit"/>
+            </form>
+            
+            </div>
+  
+        </>
         ); 
 }
